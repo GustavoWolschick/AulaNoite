da49d6f8ed1f0f0bd906acd6e023fafc3a2d315c
GustavoRW <gustavorw7@gmail.com>
2024-06-11 22:24:07 -0300

    parte 2
    criação do style e lincagem com a tela inicial do projeto

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto PHP</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <!-- div principal - pagina inteira -->
+    <div class="container">
+        <!-- ceçalho -->
+        <header class="header">
+            <h1>Projeto PHP</h1>
+        </header>
+        <br>
+        <main>
+            <h2 id="titulo">Escolha a atividade que deseja realizar</h2>
+            <!-- lista -->
+            <ul>
+                <br>
+                <li>
+                    <a href="">
+                        <h3>Rifa on-line</h3>
+                    </a>
+                    <a href="CalculadoraIMC.php">
+                        <h3>Calculadora de IMC</h3>
+                    </a>
+                    <a href="">
+                        <h3>Conversor de Moedas</h3>
+                    </a>
+                    <a href="">
+                        <h3>Calculadora de gorjetas</h3>
+                    </a>
+                    <a href="">
+                        <h3>Conversor de temperatura</h3>
+                    </a>
+                    <a href="">
+                        <h3>Calculadora de áreas</h3>
+                    </a>
+
+                </li>
+
+            </ul>
+            <br>
+        </main>
+    </div>
+    <footer>
+        Copyright Gustavo R. Wolschick | 2024
+    </footer>
+</body>
+
+</html>
